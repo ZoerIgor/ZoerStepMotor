b01e6f67bb009935fb7b72bfcecf2a0f0cd71c69
ZoerIgor <amalgama2002@mail.ru>
2021-08-18 11:57:27 +0300

    VERSION_1.0

---
+++ src/main.cpp
@@ -0,0 +1,381 @@
+#include <Arduino.h>
+#define DEBUG
+#define GEAR_RATIO 1.2
+// Модели ускорения
+  float arrAcc_0[] = {1.00, 1.00};                                                         // Move(ms): 9757 / Calculate(ms):   98 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_1[] = {0.00, 10.00, 0.00};                                                  // Move(ms): 4417 / Calculate(ms):  148 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_2[] = {0.00, 8.00, 9.50, 10.00, 9.50, 8.00, 0.00};                          // Move(ms): 3197 / Calculate(ms): 1068 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_3[] = {4.00, 8.50, 10.00, 9.20, 7.00};                                      // Move(ms): 2706 / Calculate(ms):  499 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_4[] = {3.00, 9.40, 9.80, 10.00, 9.60, 8.00, 4.00};                          // Move(ms): 2523 / Calculate(ms): 1132 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_5[] = {3.00, 9.80, 10.00, 9.60, 7.00};                                      // Move(ms): 2209 / Calculate(ms):  499 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_6[] = {4.00, 9.80, 10.00, 9.80, 8.00};                                      // Move(ms): 1996 / Calculate(ms):  499 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_7[] = {4.00, 9.50, 9.80, 10.00, 9.80, 9.50, 8.00};                          // Move(ms): 1898 / Calculate(ms): 1132 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_8[] = {4.00, 9.70, 9.80, 9.90, 10.00, 9.90, 9.80, 9.70, 8.00};              // Move(ms): 1713 / Calculate(ms): 2176 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+  float arrAcc_9[] = {4.00, 10.00, 10.00, 10.00, 10.00, 10.00, 10.00, 10.00, 10.00, 8.00}; // Move(ms): 1517 / Calculate(ms): 2883 / totalSteps = 7600 / minDelay = 80 / maxDelay = 700 /
+// Переменные
+  bool direction = false;
+  bool stop = false;
+  size_t arrAccSize = 0;
+  byte PUL=19;
+  byte DIR=18;
+  byte ENA=23;
+  byte accelerationModel = 0;
+  const byte gearsTransmission = 9;
+  float arrAccSumm = 0;
+  float currentGear = 0;
+  float defaultMinDelay = 80;
+  float defaultMaxDelay = 700;
+  float minDelay = 0;
+  float maxDelay = 0;
+  float currentDelay = 0;
+  float * arrYi;
+  float yI = 0;
+  int * arrXi;
+  int xI = 0;
+  int totalSteps = 0;
+  short int partSize = 0;
+  short int * arrDelays;
+// Инструкции
+  void InitializationOgVariables(){
+    totalSteps = 3800;
+    currentGear = 9;
+    accelerationModel = 2;
+    minDelay = 80;
+    maxDelay = 700;
+  }
+  void PrepareArrow(float arrAcc[]){
+    if(minDelay == 0 || maxDelay == 0){
+      minDelay = defaultMinDelay;
+      maxDelay = defaultMaxDelay;
+    }
+    arrXi = new int[arrAccSize];
+    arrYi = new float[arrAccSize];
+    partSize = round(totalSteps / (arrAccSize - 1));
+    for(byte i = 0; i < arrAccSize; i++)
+    {
+      if(i == 0){
+        xI = 0;
+      }else{
+        if(i == (arrAccSize - 1)){
+          xI = totalSteps;
+        }else{
+          xI = xI + partSize;
+        }
+      }
+      arrXi[i] = xI;
+      arrYi[i] = arrAcc[i];
+      arrAccSumm = arrAccSumm + arrAcc[i];
+    }
+    #ifdef DEBUG1
+      Serial.print("Part size:\t\t");
+      Serial.println(partSize);
+      Serial.print("Size arrow:\t\t");
+      Serial.println(arrAccSize);
+      Serial.print("Arrow acceleration:\t{ ");
+      for (int i = 0; i < arrAccSize; i++)
+      {
+        Serial.print(arrAcc[i]);
+        Serial.print(", ");
+      }
+      Serial.println("}");
+      Serial.print("Arrow Xi:\t\t{ ");
+      for (int i = 0; i < arrAccSize; i++)
+      {
+        Serial.print(arrXi[i]);
+        Serial.print(", ");
+      }
+      Serial.println("}");
+      Serial.print("Arrow Yi:\t\t{ ");
+      for (int i = 0; i < arrAccSize; i++)
+      {
+        Serial.print(arrYi[i]);
+        Serial.print(", ");
+      }
+      Serial.println("}");
+      Serial.println("-------------------------------");
+    #endif
+  }
+  void WriteArrayDelays(){
+    delete[] arrDelays;
+    arrDelays = new short int[totalSteps];
+    byte counter = 0;
+    byte argProdCounter = 0;
+    float arrArgNumer[arrAccSize] = {};
+    float arrArgDenom[arrAccSize - 1] = {};
+    float arrArgI[arrAccSize] = {};
+    float y = 0;
+    float numerator = 0;
+    float denominator = 0;
+    float minGearValue = (arrAccSumm / arrAccSize) / GEAR_RATIO;
+    float deltaGear = gearsTransmission - minGearValue;
+    float specificGear = deltaGear / gearsTransmission;
+    #ifdef DEBUG
+      Serial.print("Current gear:\t\t");
+      Serial.println(currentGear);
+    #endif
+    currentGear = minGearValue + (currentGear * specificGear);
+    currentDelay = maxDelay - (((maxDelay - minDelay) / gearsTransmission) * currentGear);
+    
+      Serial.print("Arrow ACC summ:\t\t");
+      Serial.println(arrAccSumm);
+      Serial.print("Arrow ACC size:\t\t");
+      Serial.println(arrAccSize);
+      Serial.print("Min gear value:\t\t");
+      Serial.println(minGearValue);
+      Serial.print("Delta gear:\t\t");
+      Serial.println(deltaGear);
+      Serial.print("Specific gear:\t\t");
+      Serial.println(specificGear);
+      Serial.print("Current gear:\t\t");
+      Serial.println(currentGear);
+      Serial.print("Current delay:\t\t");
+      Serial.println(currentDelay);
+      Serial.println("-------------------------------");
+    
+    // Обход всех X и запись всех Y в массив
+    for(int x = 1; x <= totalSteps; x++)
+    {
+      counter = 0; // Счетчик перебора Yi-х
+      // Чтение всех Yi из массива arrYi
+      for(byte i = 0; i < arrAccSize; i++)
+      {
+        yI = arrYi[counter];
+        // Запись массива i-х аргументов
+        for(byte i = 0; i < arrAccSize; i++)
+        {
+          // Запись массива с числителями i-го агрумента
+          arrArgNumer[counter] = yI; // Запись Yi-го
+          #ifdef DEBUG1
+            Serial.print("Counter:\t\t");
+            Serial.println(counter);
+            Serial.print("Yi:\t\t\t");
+            Serial.println(arrArgNumer[counter]);
+          #endif
+          for(byte i = 0; i < arrAccSize; i++) // Запись Хi-ых
+          {
+            if(i != counter){
+              arrArgNumer[i] = x - arrXi[i];
+              #ifdef DEBUG1
+                Serial.print("Counter:\t\t");
+                Serial.println(counter);
+                Serial.print("i:\t\t\t");
+                Serial.println(i);
+                Serial.print("Xi:\t\t\t");
+                Serial.println(arrArgNumer[i]);
+              #endif
+            }
+          }
+          #ifdef DEBUG1
+            Serial.print("Arrow numerator:\t{ ");
+            for (byte i = 0; i < arrAccSize; i++)
+            {
+              Serial.print(arrArgNumer[i]);
+              Serial.print(", ");
+            }
+            Serial.println("}");
+          #endif
+          // Запись массива со знаменателями i-го аргумента
+          int denom = 0;
+          int counterArrArgDenom = 0;
+          for(byte i = 0; i < arrAccSize; i++)
+          {
+            denom = arrXi[counter] - arrXi[i];
+            if(denom != 0){
+              arrArgDenom[counterArrArgDenom] = denom;
+              counterArrArgDenom++;
+            }
+          }
+          #ifdef DEBUG1
+            Serial.print("Arrow denominator:\t{ ");
+            for (byte i = 0; i < (arrAccSize - 1); i++)
+            {
+              Serial.print(arrArgDenom[i]);
+              Serial.print(", ");
+            }
+            Serial.println("}");
+          #endif
+          // Подсчет числителя
+          numerator = 1;
+          for(byte i = 0; i < arrAccSize; i++)
+          {
+            numerator = numerator * arrArgNumer[i];
+          }
+          #ifdef DEBUG1
+            Serial.print("Numerator:\t\t");
+            Serial.println(numerator);
+          #endif
+          // Подсчет знаминателя
+          denominator = 1; 
+          for(byte i = 0; i < (arrAccSize - 1); i++)
+          {
+            denominator = denominator * arrArgDenom[i];
+          }
+          #ifdef DEBUG1
+            Serial.print("Denominator:\t\t");
+            Serial.println(denominator);
+            Serial.println("-------------------------------");
+          #endif
+          // Запись i-го аргумента в массив
+          arrArgI[counter] = numerator / denominator;
+          #ifdef DEBUG1
+            Serial.print("Argument i:\t\t");
+            Serial.println(arrArgI[counter]);
+          #endif
+          #ifdef DEBUG1
+            Serial.print("i:\t\t\t");
+            Serial.println(i);
+            Serial.print("Counter:\t\t");
+            Serial.println(counter);
+            Serial.print("Optimizer counter:\t");
+            Serial.println(optimCounter);
+            Serial.println("-------------------------------");
+          #endif
+          // Расчет и запись всех Y
+          if(argProdCounter == 0){
+            y = y + arrArgI[counter];
+            if(counter == (arrAccSize - 1)){
+              arrDelays[(x-1)] = round(maxDelay -(y * (maxDelay - currentDelay)/(gearsTransmission + 1)));
+              #ifdef DEBUG1
+                Serial.print("X: ");
+                Serial.print(x);
+                Serial.print("\t\t\t");
+                Serial.print("Y: ");
+                Serial.println(y);
+              #endif
+              y = 0;
+            }
+          }
+          #ifdef DEBUG1
+            Serial.print("X:\t\t\t");
+            Serial.println(x);
+            Serial.print("Y:\t\t\t");
+            Serial.println(y);
+            Serial.print("Counter:\t\t");
+            Serial.println(counter);
+            Serial.println("-------------------------------");
+          #endif
+          argProdCounter++;
+        }
+        argProdCounter = 0;
+        counter++;
+      }
+      counter = 0;
+    }
+    #ifdef DEBUG1
+      for(int x = 1; x <= totalSteps; x++)
+      {
+        Serial.print("X: ");
+        Serial.print(x);
+        Serial.print("\t\t\t");
+        Serial.print("Y: ");
+        Serial.println(arrDelays[(x-1)]);
+      }
+    #endif
+    delete[] arrXi;
+    delete[] arrYi;
+  }
+  
+  void setup(){
+    #ifdef DEBUG
+      Serial.begin(921600);
+      Serial.println();
+      Serial.println("-------------------------------");
+    #endif
+
+    pinMode (PUL, OUTPUT);
+    pinMode (DIR, OUTPUT);
+    pinMode (ENA, OUTPUT);
+
+    InitializationOgVariables();
+
+    switch (accelerationModel)
+    {
+      case 1:
+        arrAccSize = sizeof(arrAcc_1) / sizeof(arrAcc_1[0]);
+        PrepareArrow(arrAcc_1);
+      break;
+      case 2:
+        arrAccSize = sizeof(arrAcc_2) / sizeof(arrAcc_2[0]);
+        PrepareArrow(arrAcc_2);
+      break;
+      case 3:
+        arrAccSize = sizeof(arrAcc_3) / sizeof(arrAcc_3[0]);
+        PrepareArrow(arrAcc_3);
+      break;
+      case 4:
+        arrAccSize = sizeof(arrAcc_4) / sizeof(arrAcc_4[0]);
+        PrepareArrow(arrAcc_4);
+      break;
+      case 5:
+        arrAccSize = sizeof(arrAcc_5) / sizeof(arrAcc_5[0]);
+        PrepareArrow(arrAcc_5);
+      break;
+      case 6:
+        arrAccSize = sizeof(arrAcc_6) / sizeof(arrAcc_6[0]);
+        PrepareArrow(arrAcc_6);
+      break;
+      case 7:
+        arrAccSize = sizeof(arrAcc_7) / sizeof(arrAcc_7[0]);
+        PrepareArrow(arrAcc_7);
+      break;
+      case 8:
+        arrAccSize = sizeof(arrAcc_8) / sizeof(arrAcc_8[0]);
+        PrepareArrow(arrAcc_8);
+      break;
+      case 9:
+        arrAccSize = sizeof(arrAcc_9) / sizeof(arrAcc_9[0]);
+        PrepareArrow(arrAcc_9);
+      break;
+      default:
+        arrAccSize = sizeof(arrAcc_0) / sizeof(arrAcc_0[0]);
+        PrepareArrow(arrAcc_0);
+      break;
+    }
+    WriteArrayDelays();
+    #ifdef DEBUG
+      Serial.print("Boot time:\t\t");
+      Serial.print(millis());
+      Serial.println(" ms");
+      Serial.println("-------------------------------");
+    #endif
+    delay(2000);
+  }
+// Движение
+  int Move(bool direction, int totalSteps){
+    int x = 0;
+    #ifdef DEBUG
+      Serial.println("START OF MOVEMENT");
+      Serial.print("Total steps:\t\t");
+      Serial.println(totalSteps);
+      Serial.println("-------------------------------");
+      int time = millis();
+    #endif
+    digitalWrite(DIR,direction);
+    digitalWrite(ENA,HIGH);
+    for(; x <= totalSteps; x++)
+    {
+      digitalWrite(PUL,HIGH);
+      delayMicroseconds(arrDelays[x]);
+      digitalWrite(PUL,LOW);
+      delayMicroseconds(arrDelays[x]);
+      if(stop == true){
+        break;
+      }
+    }
+    digitalWrite(ENA,LOW);
+    #ifdef DEBUG
+      time = millis() - time;
+      Serial.println("END OF MOVEMENT");
+      Serial.print("Total time:\t\t");
+      Serial.print(time);
+      Serial.println(" ms");
+      Serial.println("-------------------------------");
+    #endif 
+    return x;
+  }
+  void loop(){
+    Move(true, totalSteps);
+    delay(2000);
+    Move(false, totalSteps);
+    delay(2000);
+  }
